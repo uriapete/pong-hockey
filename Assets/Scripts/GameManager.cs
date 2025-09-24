c0ee59f2e46ab92c4deb8ae91c5d49c35e3ac50b
jay-uliam <pursuit.school.julia@gmail.com>
2025-09-24 15:55:14 -0700

    init gamemanager score and winning score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
+using System.Diagnostics;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int score {get; private set;} 
+    int winningScore = 7;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        score = 0;
     }
 
     // Update is called once per frame
@@ -13,4 +18,6 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    
 }
