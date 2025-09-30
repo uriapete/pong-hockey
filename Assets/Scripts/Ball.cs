f1346313c30d8d31f518bacd81a78e2dc54fcecb
uriapete <uriarteangelo@gmail.com>
2025-09-30 14:30:53 -0700

    Ball.cs - var ballRB

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public float Speed { get; private set; } = 5;
+
+    // the RB node for the ball is here.
+    Rigidbody2D ballRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // get the ball's RB.
+        ballRB = this.GetComponent<Rigidbody2D>();
     }
 
     void Serve()
