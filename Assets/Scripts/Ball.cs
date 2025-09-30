f916259245a3fd061e3c1f203dd38ee9edb5a3ef
uriapete <uriarteangelo@gmail.com>
2025-09-30 13:45:08 -0700

    Ball.cs - new field Speed

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float Speed { get; private set; } = 5;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
