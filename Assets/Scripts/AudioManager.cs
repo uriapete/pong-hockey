5a32e8f49df3d3ac12017425b3bf4ae2e0d48e6d
Maria Trezha Von Bargen <vonbargen.trezha@gmail.com>
2025-10-31 14:49:23 -0700

    Add AudioManager and background music to scene
    Added AudioManager script to handle music and SFX playback. Added a background music audio clip and configured PlayScene with Music and SFX AudioSources for audio management.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+    [Header("Audio Clip")]
+    public AudioClip backgroundMusic;
+    public AudioClip paddleHitSFX;
+
+    void Start()
+    {
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
+    }
+}
