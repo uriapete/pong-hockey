d7b0f5f491b0eb63bf845f3fe289bd17bd2a879a
Maria Trezha Von Bargen <vonbargen.trezha@gmail.com>
2025-11-11 16:22:43 -0800

    Dropdown reflects all res options

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -6,8 +6,8 @@ using TMPro;
 
 public class SettingsManager : MonoBehaviour
 {
-    public Dropdown resolutionDropdown;
-    Resolution[] resolutions; //array for resolution options
+    public TMP_Dropdown resolutionDropdown; //references UnityEngine.UI element 
+    Resolution[] resolutions; //list of resolution options
 
     public void SetFullScreen(bool isFullScreen) //checks if the game is fullscreen for the toggle 
     {
@@ -16,28 +16,28 @@ public class SettingsManager : MonoBehaviour
     
     void Start()
     {
-        resolutions = Screen.resolutions;
+        resolutions = Screen.resolutions; // Screen.resolutions accesses unity's list of available screen resolutions
 
         resolutionDropdown.ClearOptions(); //clears out all options in the dropdown initially
 
         List<string> options = new List<string>(); // creates a list of strings (the options)
 
-        int currentResolutionIndex = 0;
+        //int currentResolutionIndex = 0;
 
         for (int i = 0; i < resolutions.Length; i++)// loop through each element in the resolutions array
         {
             string option = resolutions[i].width + " x " + resolutions[i].height; //create a nicely formatted string that displays resolution
             options.Add(option); //add to options list
 
-            if (resolutions[i] == Screen.currentResolution.width && 
-            resolutions[i].height == Screen.currentResolution.height)
+            //if (resolutions[i] == Screen.currentResolution.width && 
+            //resolutions[i].height == Screen.currentResolution.height)
             {
-                currentResolutionIndex = i;
+                //currentResolutionIndex = i;
             }
         }
 
-        resolutionDropdown.AddOptions(); //add the options list to the dropdown)
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+        resolutionDropdown.AddOptions(options); //add the options list to the dropdown (note: takes in strings not arrays)
+        //resolutionDropdown.value = currentResolutionIndex;
+        //resolutionDropdown.RefreshShownValue();
     }
 }
