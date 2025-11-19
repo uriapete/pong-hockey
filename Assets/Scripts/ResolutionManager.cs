5247ac2e53f919c5ad5b6b8a6f17911c5f66f4fb
Maria Trezha Von Bargen <vonbargen.trezha@gmail.com>
2025-11-19 00:35:44 -0800

    Most Stable Resolution Commit so far
    Full screen toggle doesn't work yet.
    Paddles are off screen.
    But resolution changing and dropdown UI works.
    
    Almost there :')

--- Assets/Scripts/ResolutionManager.cs
+++ Assets/Scripts/ResolutionManager.cs
@@ -2,62 +2,92 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
+using System.Linq;
 
 public class ResolutionManager : MonoBehaviour
 {
+    [Header("Drop down UI Elements to be messed with >:)")]
     [SerializeField] private TMP_Dropdown resolutionDropdown; 
     [SerializeField] private Toggle fullscreenToggle;
 
-    private Resolution[] resolutions;
-    private List<Resolution> filteredResolutions;
-    private int currentResolutionIndex;
+    
+    private Resolution[] filteredResolutions; // array that stores filtered resolutions
+    private List<string> resolutionOptions = new List<string>(); // list of strings for the dropdown UI
+
 
     void Start()
     {
-        SetupResolutions();
+        FullscreenToggle();
+        //
     }
 
-    private void SetupResolutions()
+    
+    void Awake()
     {
-        resolutions = Screen.resolutions;
-        filteredResolutions = new List<Resolution>();
+        SetupAvailableResolutions();
+        //
+    }
+
+    void FullscreenToggle()
+    {
+        //
+    }
+
+    void SetupAvailableResolutions()
+    {
+        Resolution[] allResolutions = Screen.resolutions; // populate array w/ all available resolutions (width, height, refreshRate)
+    
+
+        filteredResolutions = allResolutions.GroupBy(res => new {res.width, res.height,}).Select(g => g.First()).ToArray(); 
+        //^^LINQ can filter out duplicate widths/heights by keeping only distinct values.^^
+
         resolutionDropdown.ClearOptions();
+        resolutionOptions.Clear();
+        
+        int currentResolutionIndex = 0;
 
-        int currentRefreshRate = (int)Screen.currentResolution.refreshRateRatio.value;
-
-        foreach (var res in resolutions) 
+        //iterate through every filtered array and format them
+        for (int i = 0; i < filteredResolutions.Length; i++)
         {
-            if ((int)res.refreshRateRatio.value == currentRefreshRate)
-                filteredResolutions.Add(res);
-        }
-
-        if (filteredResolutions.Count == 0)
-            filteredResolutions.AddRange(resolutions);
-
-        List<string> options = new List<string>();
-        for (int i = 0; i < filteredResolutions.Count; i++)
-        {
-            var res = filteredResolutions[i];
-            options.Add(res.width + " x " + res.height);
-
-            if (res.width == Screen.width && res.height == Screen.height)
+            string option = filteredResolutions[i].width + "x" + filteredResolutions[i].height;
+            resolutionOptions.Add(option); // Add the list of strings
+            
+            // also determine which resolution the game is currently using
+            if (filteredResolutions[i].width == Screen.currentResolution.width && 
+                filteredResolutions[i].height == Screen.currentResolution.height)
+            {
                 currentResolutionIndex = i;
+            }
         }
 
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = Mathf.Clamp(currentResolutionIndex, 0, filteredResolutions.Count - 1);
+        resolutionDropdown.AddOptions(resolutionOptions);
+        
+        resolutionDropdown.onValueChanged.RemoveListener(ApplyResolution); // fix for nulling race condiiton issue
+
+        resolutionDropdown.value = currentResolutionIndex; // need to sneak this bad boy in so Unity doesn't bark at it
+
+        resolutionDropdown.onValueChanged.AddListener(ApplyResolution); // end of fix
+        
         resolutionDropdown.RefreshShownValue();
+
+    }
+    public void ApplyResolution(int resolutionIndex)
+    {
+        if (filteredResolutions == null || filteredResolutions.Length == 0) //don't get rid of this, trust me.
+        {
+            //Debug.LogWarning("flipping race condition: resolutions not initialized yet.");
+            return; // Exit safely if data isn't ready
+        }
+
+        if (resolutionIndex >= 0 && resolutionIndex < filteredResolutions.Length) 
+        {
+            Resolution selectedResolution = filteredResolutions[resolutionIndex];
+            
+            Screen.SetResolution(selectedResolution.width, selectedResolution.height, FullScreenMode.Windowed);
+            
+            Debug.Log($"Changed resolution to: {selectedResolution.width}x{selectedResolution.height}");
+        
+        }
     }
 
-    public void SetResolution(int index)
-    {
-        var res = filteredResolutions[index];
-        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
-        currentResolutionIndex = index;
-    }
-
-    public void SetFullScreen(bool isFullScreen)
-    {
-        Screen.fullScreen = isFullScreen;
-    }
 }
