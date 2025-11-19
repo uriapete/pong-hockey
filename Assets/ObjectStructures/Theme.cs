183b9a268133af461f68cc4f5c41a18bd4b060b5
uriapete <uriarteangelo@gmail.com>
2025-11-19 15:39:22 -0800

    Theme.cs - add field name

--- Assets/ObjectStructures/Theme.cs
+++ Assets/ObjectStructures/Theme.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Theme", menuName = "Scriptable Objects/Theme")]
 public class Theme : ScriptableObject
 {
+    // name of the theme.
+    public string Name;
+
     // The Sprites that will be used for the specified objects.
     public SpriteRenderer Background;
     public SpriteRenderer Ball;
