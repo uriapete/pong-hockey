858860107d02573c418d2e05fc22091079179b71
uriapete <uriarteangelo@gmail.com>
2025-11-19 15:55:52 -0800

    TM - new override PickTheme(Theme)

--- Assets/Scripts/ThemeManager.cs
+++ Assets/Scripts/ThemeManager.cs
@@ -67,7 +67,11 @@ public class ThemeManager : MonoBehaviour
         }
     }
 
-
+    // takes a theme, implements into play
+    public void PickTheme(Theme theme)
+    {
+        
+    }
     public void PickTheme(int themeId)  //each theme corresponds to an int, so input int to change theme
     {
         switch (themeId)
