286048c0d17a41812ca5b36d2b600f0a646788d0
uriapete <uriarteangelo@gmail.com>
2025-11-19 15:36:37 -0800

    Theme.cs - define fields

--- Assets/ObjectStructures/Theme.cs
+++ Assets/ObjectStructures/Theme.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Theme", menuName = "Scriptable Objects/Theme")]
 public class Theme : ScriptableObject
 {
-    
+    // The Sprites that will be used for the specified objects.
+    public SpriteRenderer Background;
+    public SpriteRenderer Ball;
+    public SpriteRenderer PaddleR;
+    public SpriteRenderer PaddleL;
 }
