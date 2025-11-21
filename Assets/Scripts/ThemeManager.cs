90bca27be01cac4bc80e8afa003a3791de709fbb
uriapete <uriarteangelo@gmail.com>
2025-11-20 22:33:19 -0800

    TM - comment out sound stuff for now to shut the compiler up

--- Assets/Scripts/ThemeManager.cs
+++ Assets/Scripts/ThemeManager.cs
@@ -37,12 +37,12 @@ public class ThemeManager : MonoBehaviour
     // temporary id variable for pre-ui testing.
     private int TEMPThemeID = 0;
 
-   [Header("Sound: [Music, SFX]")]
-   private SOUND[] chosenSound;
+//    [Header("Sound: [Music, SFX]")]
+//    private SOUND[] chosenSound;
 
-   public SOUND[] defaultSound;
-   public SOUND[] goldSound;
-   public SOUND[] halloweenSound;
+//    public SOUND[] defaultSound;
+//    public SOUND[] goldSound;
+//    public SOUND[] halloweenSound;
 
    
 
