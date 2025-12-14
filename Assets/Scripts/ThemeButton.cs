a10d670841a2fff4fe7f7053f1213d4c07b84718
uriapete <uriarteangelo@gmail.com>
2025-12-13 20:15:10 -0800

    Scripts/ThemeButton.cs - Start - add todo: load thumbnail

--- Assets/Scripts/ThemeButton.cs
+++ Assets/Scripts/ThemeButton.cs
@@ -20,7 +20,7 @@ public class ThemeButton : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // TODO: On Start, load thumbnail onto button.
     }
 
     // Update is called once per frame
