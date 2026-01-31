e931cfe4726487707f4cea8f61e3bf927012d989
uriapete <uriarteangelo@gmail.com>
2026-01-31 13:32:11 -0800

    Scripts/ThemesMenu.cs - LoadThemes on Start

--- Assets/Scripts/ThemesMenu.cs
+++ Assets/Scripts/ThemesMenu.cs
@@ -14,7 +14,7 @@ public class ThemesMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        LoadThemes();
     }
 
     // Update is called once per frame
