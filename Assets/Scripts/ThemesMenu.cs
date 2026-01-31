3bf66d4f870b7cdff0a7ab28319aebe1c387857e
uriapete <uriarteangelo@gmail.com>
2026-01-31 14:23:08 -0800

    Scripts/ThemesMenu.cs - remove pseudocode

--- Assets/Scripts/ThemesMenu.cs
+++ Assets/Scripts/ThemesMenu.cs
@@ -47,26 +47,6 @@ public class ThemesMenu : MonoBehaviour
     void LoadThemes(int startIdx = 0)
     {
         CurrentStartIdx = startIdx;
-        // TODO: Implement pseudocode.
-
-        // Take ThemeManager.Themes[]
-        // Take themes from Themes @ idxs: startIdx, startIdx+1, ...+2 -> themesToLoad[] (length<=3)
-
-        // determine start position: switch (length)
-            // case 0: return
-            // case 1: 0,0
-            // case 2: light left,
-                // each button takes left or right
-            // case 3: left
-                // left, center, right
-        // finish -> start_x
-
-        // For each theme,idx in themesToLoad,
-            // construct a ThemeButton, add theme as arg -> thmBtn
-            // position in correct position (left, right, or center?) (or for 2, left or right?)
-                // thmBtn.x = start_x + (idx*offset)
-            // add to tree under this (ThemesMenu)
-        // next
         
         // for each button on the menu...
         for (int i = 0; i < Buttons.Length; i++)
